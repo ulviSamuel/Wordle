7978260cbb139e322ba37cfcfcd0933d5b7503fa
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-03-02 14:50:31 +0100

    Non va

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   $res = mysqli_query($con, $sql);
   $row = mysqli_fetch_array($res);
   $_SESSION['idParolaDaTrovare'] = $row[0];*/
+  session_start();
 ?>
 
 <html>
@@ -14,7 +15,7 @@
   <body>
       <h1 id='titolo'>Word?e</h1>
       <p id="vecchieParole"></p>
-      <input placeholder="Inserisci una parola di 5 caratteri" type="text" name="nuovaParola" id="nuovaParola" maxlength="5">
+      <input onchange="testoScritto()" placeholder="Inserisci una parola di 5 caratteri" type="text" name="nuovaParola" id="nuovaParola" maxlength="5">
       <button id='invioBtn' onclick="clickBtnCerca()">Invio</button>
 
       <script>
@@ -44,6 +45,35 @@
             else
               alert("La parola deve essere lunga 5 caratteri!");
           }
+        }
+
+        function testoScritto()
+        {
+          /*var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+              var pulsanteInvio = document.getElementById("invioBtn");
+              if(<?php echo $_SESSION['parolaEsistente']; ?> === "non esiste")
+              {
+                pulsanteInvio.style.backgroundColor = "red";
+                pulsanteInvio.style.pointerEvents = "none";
+              }
+              else
+              {
+                pulsanteInvio.style.backgroundColor = "#04b304";
+                pulsanteInvio.style.pointerEvents = "auto";
+                pulsanteInvio.addEventListener("mouseover", function() {
+                  pulsanteInvio.style.backgroundColor = "green";
+                });
+                pulsanteInvio.addEventListener("mouseout", function() {
+                  pulsanteInvio.style.backgroundColor = "#04b304";
+                });
+              }
+            }
+          };
+          xhttp.open("GET", "verifica_esistenza_parola.php?nuovaParola="+nuovaParola, true);
+          xhttp.send();*/
         }
       </script>
 
--- verifica_esistenza_parola.php
+++ verifica_esistenza_parola.php
@@ -1,4 +1,9 @@
 <?php   
+    if(isset($_REQUEST['nuovaParola']))
+    {
+        session_start();
+        $nuovaParola = $_REQUEST['nuovaParola'];
+    }
     /*$sql = "CALL testVocabolario('$nuovaParola')";
     $res = mysqli_query($con, $sql);
     $row = mysqli_fetch_array($res);
@@ -6,5 +11,14 @@
         $parolaEistente = true;
     else
         $parolaEistente = false;*/
-        $parolaEistente = true;
+    $randomNumber = rand(0, 1);
+    $result = ($randomNumber == 1) ? true : false;
+    $parolaEistente = $result;
+    if(isset($_REQUEST['nuovaParola']))
+    {
+        if($parolaEistente == false)
+            $_SESSION['parolaEsistente'] = "non esiste";
+        else
+            $_SESSION['parolaEsistente'] = "esiste";
+    }
 ?>
